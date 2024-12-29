9e59a84d1c742525971604f63642c281f796be67
irakli adamashvili <adamashvili.irakli8@gmail.com>
2024-12-30 01:12:11 +0400

    filter

--- src/app/services/products-area.service.ts
+++ src/app/services/products-area.service.ts
@@ -1,41 +1,71 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AllProductArea } from '../../interfaces/all-product-area';
+import { FilteredProducts } from '../../interfaces/filtered-products';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsAreaService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getCategories() {
-    return this.http.get("https://api.everrest.educata.dev/shop/products/categories")
+    return this.http.get(
+      'https://api.everrest.educata.dev/shop/products/categories'
+    );
   }
 
-  getListByCategory(id: any, page:any, size :any) {
-    return this.http.get(`https://api.everrest.educata.dev/shop/products/category/${id}?page_index=${page}&page_size=${size}`)
+  getListByCategory(id: any, page: any, size: any) {
+    return this.http.get(
+      `https://api.everrest.educata.dev/shop/products/category/${id}?page_index=${page}&page_size=${size}`
+    );
   }
-
- 
 
   getBrands() {
-    return this.http.get("https://api.everrest.educata.dev/shop/products/brands")
+    return this.http.get<string[]>(
+      'https://api.everrest.educata.dev/shop/products/brands'
+    );
   }
 
   getExactBrandData(name: string) {
-    return this.http.get<AllProductArea>(`https://api.everrest.educata.dev/shop/products/brand/${name}?page_size=10`)
+    return this.http.get<AllProductArea>(
+      `https://api.everrest.educata.dev/shop/products/brand/${name}?page_size=10`
+    );
   }
 
-  getSearchedData(searchInput: string) {
-    return this.http.get(`https://api.everrest.educata.dev/shop/products/search?keywords=${searchInput}`)
+  getSearchedData(searchInput: string, size: number) {
+    return this.http.get<FilteredProducts>(
+      `https://api.everrest.educata.dev/shop/products/search?page_size=${size}&keywords=${searchInput}`
+    );
+  }
+
+  filterData(
+    searchText: string = '',
+    rating: number | string = '',
+    min: string = '1',
+    max: string = '99999',
+    type: string,
+    sort: string,
+    size: number
+  ) {
+    let isRated = rating != '' ? '&rating=' : '';
+    let isChosenType = type != '' ? '&sort_by=' : '';
+    let isSorted = sort != '' ? '&sort_direction=' : '';
+
+    return this.http.get<FilteredProducts>(
+      `https://api.everrest.educata.dev/shop/products/search?page_size=${size}&keywords=${searchText}${isRated}${rating}&price_min=${min}&price_max=${max}${isChosenType}${type}${isSorted}${sort}`
+    );
   }
 
   getCardsforHome() {
-    return this.http.get<AllProductArea>('https://api.everrest.educata.dev/shop/products/all?page_size=10')
+    return this.http.get<AllProductArea>(
+      'https://api.everrest.educata.dev/shop/products/all?page_size=10'
+    );
   }
 
   getCardsOnShopPage(page: any, size: any) {
-    return this.http.get<AllProductArea>(`https://api.everrest.educata.dev/shop/products/all?page_index=${page}&page_size=${size}`)
+    return this.http.get<AllProductArea>(
+      `https://api.everrest.educata.dev/shop/products/all?page_index=${page}&page_size=${size}`
+    );
   }
 }
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -4,6 +4,7 @@ import { ProductsAreaService } from '../services/products-area.service';
 import { FormsModule } from '@angular/forms';
 import { Product } from '../../interfaces/product';
 import { AllProductArea } from '../../interfaces/all-product-area';
+import { FilteredProducts } from '../../interfaces/filtered-products';
 
 @Component({
   selector: 'app-shop',
@@ -29,6 +30,16 @@ export class ShopComponent implements OnInit {
   public altImage: string =
     'https://media.istockphoto.com/id/1396814518/vector/image-coming-soon-no-photo-no-thumbnail-image-available-vector-illustration.jpg?s=612x612&w=0&k=20&c=hnh2OZgQGhf0b46-J2z7aHbIWwq8HNlSDaNp2wn_iko=';
 
+  pagination(data: FilteredProducts | AllProductArea) {
+    this.pageList = [];
+    this.productList = data.products;
+    let pages = Math.ceil(data.total / this.pageSize);
+
+    for (let i = 1; i <= pages; i++) {
+      this.pageList.push(i);
+    }
+  }
+
   showProducts(page: number | string = 1, size: number = this.totalSize) {
     this.isCategoryShown = false;
     this.currentPage = +page;
@@ -37,11 +48,33 @@ export class ShopComponent implements OnInit {
       .subscribe((data: AllProductArea) => {
         this.productList = data.products;
         this.totalSize = data.total;
-        let pages = Math.round(data.total / size);
-        this.pageList = [];
-        for (let i = 1; i <= pages; i++) {
-          this.pageList.push(i);
-        }
+        this.pagination(data);
+      });
+  }
+
+  search(search: string) {
+    this.prodService
+      .getSearchedData(search, this.pageSize)
+      .subscribe((data: FilteredProducts) => {
+        this.productList = data.products;
+        this.pagination(data);
+      });
+  }
+
+  filterProducts(info: any) {
+    this.prodService
+      .filterData(
+        (info.search = ''),
+        info.rating,
+        (info.min = '1'),
+        (info.max = '999'),
+        info.type,
+        info.sort,
+        this.pageSize
+      )
+      .subscribe((data: FilteredProducts) => {
+        this.productList = data.products;
+        this.pagination(data);
       });
   }
 
@@ -92,15 +125,9 @@ export class ShopComponent implements OnInit {
     this.prodService
       .getListByCategory(category, this.currentPage, this.pageSize)
       .subscribe((data: any) => {
-        console.log(data);
+  
         this.productList = data.products;
-        this.pageList = [];
-        this.productList = data.products;
-        let pages = Math.round(data.total / this.pageSize);
-
-        for (let i = 1; i <= pages; i++) {
-          this.pageList.push(i);
-        }
+        this.pagination(data);
       });
   }
 }
